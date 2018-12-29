999bd26526e37be8d5ddb5a94a7966adb18034d3
ldh8888 <3128838441@qq.com>
2018-12-29 12:29:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ function googleAnalyticsMiddleware(data) {
         // https://nodejs.org/api/stream.html#stream_transform
         data.stream = data.stream.pipe(new Transform({
             decodeStrings: false,
-            defaultEncoding:'gb2312',
             transform: function(chunk, encoding, next) {
                 this.push(addGa(chunk.toString()));
                 next();
